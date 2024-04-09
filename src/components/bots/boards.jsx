9dee745bb93d28e06220f9406b72ffc5e3972b67
daviddflix <daviddflix@gmail.com>
2024-04-09 18:31:58 -0300

    fa bot

--- src/components/bots/boards.jsx
+++ src/components/bots/boards.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CircularProgress from '@mui/material/CircularProgress';
+import { useParams } from 'react-router-dom';
 
 const BASE_URL_MONDAY_BOT = import.meta.env.VITE_NOVATIDE_MONDAY_BOT_URL;
 
 const Boards = () => {
-
     const [boards, setBoards] = useState([]);
-
+    const { botName } = useParams();
+    
     // Function to fetch all Monday Boards
     useEffect(() => {
         const getBoards = async () => {
@@ -28,29 +29,37 @@ const Boards = () => {
         };
 
         getBoards();
-    }, []);
+    }, []); 
 
     return (
         <div className="boards-container">
-            <h1 className='monday-boards-title'>Monday Boards</h1>
-            <div>
-                {boards.length === 0 ? (
-                    <div className="loader-container">
-                        <CircularProgress />
+            {botName && botName === 'fundamental analysis' ? (
+                <div className='fundamental-analysis-container'>
+                    <h2 className='fa-title'>{botName}</h2>
+                    <h4>No available information yet</h4>
+                </div>
+            ) : (
+                <React.Fragment>
+                    <h1 className='monday-boards-title'>Monday Boards</h1>
+                    <div>
+                        {boards.length === 0 ? (
+                            <div className="loader-container">
+                                <CircularProgress />
+                            </div>
+                        ) : (
+                            <div className="boards">
+                                {boards.map((item, index) => (
+                                    <a href={`https://novatidelabs-company.monday.com/boards/${item.monday_board_id}`} key={index} className="board-item">
+                                        <p className="board-name">{item.board_name}</p>
+                                        <p className="board-id">{item.monday_board_id}</p>
+                                        <p className="created-at">{item.created_at.substring(0, 16)}</p>
+                                    </a>
+                                ))}
+                            </div>
+                        )}
                     </div>
-                ) : (
-                    <div className="boards">
-                        {boards.map((item, index) => (
-                            <a href={`https://novatidelabs-company.monday.com/boards/${item.monday_board_id}`} key={index} className="board-item">
-                                <p className="board-name">{item.board_name}</p>
-                                <p className="board-id">{item.monday_board_id}</p>
-                                <p className="created-at">{item.created_at.substring(0, 16)}</p>
-                            </a>
-                        ))}
-                    </div>
-                )}
-            </div>
-
+                </React.Fragment>
+            )}
         </div>
     );
 };
--- src/components/bots/bots.jsx
+++ src/components/bots/bots.jsx
@@ -1,21 +1,115 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import '../general.css'; 
 import CircularProgress from '@mui/material/CircularProgress';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import { useEffect } from 'react';
+
 
 const BASE_URL_MONDAY_BOT = import.meta.env.VITE_NOVATIDE_MONDAY_BOT_URL;
+const BASE_URL_NOVATIDE_URL = import.meta.env.VITE_API_URL;
 
 
+const BotItem = ({ item, refreshBots  }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [newInterval, setNewInterval] = useState('');
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  const [snackbarSeverity, setSnackbarSeverity] = useState('success');
 
-const BotItem = ({ item }) => {
-  const botLink = `/bots/${item.name}`;
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleChangeStatus = async () => {
+    try {
+      const command = item.status ? 'deactivate' : 'activate'
+      const response = await axios.post(`${BASE_URL_NOVATIDE_URL}/multi-bot?command=${command}&bot_id=${item.id}`);
+      setSnackbarMessage(response.data.response);
+      setSnackbarSeverity(response.data.success ? 'success' : 'error');
+      setSnackbarOpen(true);
+      refreshBots()
+    } catch (error) {
+      console.error('Error changing bot status:', error.message);
+      setSnackbarMessage('Error changing bot status');
+      setSnackbarSeverity('error');
+      setSnackbarOpen(true);
+    }
+  };
+
+  const handleEditInterval = async () => {
+    try {
+      const response = await axios.post(`${BASE_URL_NOVATIDE_URL}/multi-bot/edit-interval?bot_id=${item.id}&interval=${newInterval}`);
+      setSnackbarMessage(response.data.response);
+      setSnackbarSeverity(response.data.success ? 'success' : 'error');
+      setSnackbarOpen(true);
+      refreshBots()
+    } catch (error) {
+      if (error.response){
+        console.error('Error editing interval:', error.response.data.response);
+        setSnackbarMessage(error.response.data.response);
+        setSnackbarSeverity('error');
+        setSnackbarOpen(true);
+      } else {
+        console.error('Error editing interval:', error.message);
+        setSnackbarMessage(error.message);
+        setSnackbarSeverity('error');
+        setSnackbarOpen(true);
+      }
+     
+    }
+  };
+
+  const handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setSnackbarOpen(false);
+  };
 
   return (
-    <Link to={botLink} className='bot-link'>
-      <div className='bot-item'>
+    <div className='bot-item'>
+      <div className="bot-options">
+        <IconButton
+          aria-controls="bot-options-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="bot-options-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem disabled={item.name === 'nv invest'} onClick={() => setOpenDialog(true)}>Edit Interval</MenuItem>
+          <MenuItem disabled={item.name === 'nv invest'} onClick={handleChangeStatus}>{item.status ? 'Deactivate' : 'Activate'}</MenuItem>
+        </Menu>
+      </div>
+      <Link to={`/bots/${item.name}`} className='bot-link'>
         <h2 className='bot-name'>{item.name}</h2>
         <p className='bot-description'>{item.description}</p>
+        <p className='bot-description'>Runs every {item.interval} hours</p>
         <div className='bot-details'>
           <p className={`bot-status ${item.status ? 'active' : 'inactive'}`}>
             {item.status ? 'Active' : 'Inactive'}
@@ -27,42 +121,95 @@ const BotItem = ({ item }) => {
             <p className='bot-updated'>{item.updated_at}</p>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+
+      <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+        <DialogTitle>Edit Interval</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="interval"
+            label="New Interval (hours)"
+            type="number"
+            fullWidth
+            value={newInterval}
+            onChange={(e) => setNewInterval(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpenDialog(false)}>Cancel</Button>
+          <Button onClick={handleEditInterval}>Submit</Button>
+        </DialogActions>
+      </Dialog>
+
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <MuiAlert elevation={6} variant="filled" severity={snackbarSeverity}>
+          {snackbarMessage}
+        </MuiAlert>
+      </Snackbar>
+    </div>
   );
 };
 
+
 const Bots = () => {
+
   const [bots, setBots] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  // Get all Novatide Bots
-  useEffect(() => {
-    const getBots = async () => {
-      try {
-        const response = await axios.get(`${BASE_URL_MONDAY_BOT}/bots`, {
-          headers: {
-            'Content-Type': 'application/json',
-          }
-        });
-
-        if (response.status === 200) {
-          const data = response.data;
-          setBots(data.bots);
+  const getBots = async () => {
+    try {
+      const novatideResponse = await axios.get(`${BASE_URL_NOVATIDE_URL}/bots`, {
+        headers: {
+          'Content-Type': 'application/json',
         }
-      } catch (error) {
-        console.error('Error fetching gas price:', error);
-      }
-    };
+      });
 
+      const mondayBotResponse = await axios.get(`${BASE_URL_MONDAY_BOT}/bots`, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      if (novatideResponse.status === 200 && mondayBotResponse.status === 200) {
+        const novatideBots = novatideResponse.data.bots;
+        const mondayBots = mondayBotResponse.data.bots;
+        const mergedBots = [...novatideBots, ...mondayBots];
+        setBots(mergedBots);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error('Error fetching bots:', error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
     getBots();
   }, []);
 
   return (
     <div className='bots-container'>
       <div className='bots-subcontainer'>
-        {bots.length > 0 ? bots.map((item, index) => (
-          <BotItem key={index} item={item} />
-        )): <div className='loader-container'>{<CircularProgress />}</div>  }
+        {loading ? (
+          <div className='loader-container'>
+            <CircularProgress />
+          </div>
+        ) : (
+          bots.length > 0 ? (
+            bots.map((item, index) => (
+              <BotItem key={index} item={item} refreshBots={getBots} />
+            ))
+          ) : (
+            <p>No bots found.</p>
+          )
+        )}
       </div>
     </div>
   );
